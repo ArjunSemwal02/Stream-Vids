d83d750c1690c6a3210db9b542a225ad05c6cec8
Arjun <arjunsemwal02@gmail.com>
2024-12-18 20:40:12 +0530

    created Category pills component and added it to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import { CategoryPills } from "./components/CategoryPills";
 import { PageHeader } from "./layouts/PageHeader";
 
 export default function App() {
 return <div className="max-h-screen flex flex-col">
-  <PageHeader/> 
+  <PageHeader/>
+  <div className="grid grid-cols-[auto, 1fr] flex-grow-1 overflow-auto">
+    <div className="">Sidebar</div>
+    <div className="sticky top-0 z-10 bg-white pb-2">
+        <CategoryPills/>
+    </div>
+  </div>
+  
 </div>
 }
