7db25ee7a15c8342624f20b4bc2b5304cd5ee624
Arjun <arjunsemwal02@gmail.com>
2024-12-18 23:19:25 +0530

    added data for category component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,14 @@
 import { CategoryPills } from "./components/CategoryPills";
+import { categories } from "./data/home";
 import { PageHeader } from "./layouts/PageHeader";
 
 export default function App() {
 return <div className="max-h-screen flex flex-col">
   <PageHeader/>
-  <div className="grid grid-cols-[auto, 1fr] flex-grow-1 overflow-auto">
+  <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
     <div className="">Sidebar</div>
     <div className="sticky top-0 z-10 bg-white pb-2">
-        <CategoryPills/>
+        <CategoryPills categories={categories}/>
     </div>
   </div>
   
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,3 +1,15 @@
-export function CategoryPills() {
-    return 1
+import { Button } from "./Button";
+
+type CategoryPillsProps = {
+    categories: string[]
+}
+
+export function CategoryPills({ categories }: CategoryPillsProps) {
+    return <div className="overflow-x-hidden relative">
+        <div className="flex whitespace-nowrap gap-2 transition-transform w-[max-content]">
+            {categories.map(category => (
+                <Button key={category} variant="dark" className="py-1 px-3 rounded-lg whitespace-nowrap">{category}</Button>
+            ))}
+        </div>
+    </div>
 }
