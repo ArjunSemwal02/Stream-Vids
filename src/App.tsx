d6ea25e774ee38899b5792aa6fe6c972c264b0a7
Arjun <arjunsemwal02@gmail.com>
2024-12-19 21:48:38 +0530

    changed the default variants for the category buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { useState } from "react";
 import { CategoryPills } from "./components/CategoryPills";
 import { categories } from "./data/home";
 import { PageHeader } from "./layouts/PageHeader";
 
 export default function App() {
+const [selectedCategory, setSelectedCategory] = useState(categories[0])
+
 return <div className="max-h-screen flex flex-col">
   <PageHeader/>
   <div className="grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto">
     <div className="">Sidebar</div>
     <div className="sticky top-0 z-10 bg-white pb-2">
-        <CategoryPills categories={categories}/>
+        <CategoryPills categories={categories} selectedCategory={selectedCategory} onSelect={setSelectedCategory}/>
     </div>
   </div>
   
--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -2,13 +2,15 @@ import { Button } from "./Button";
 
 type CategoryPillsProps = {
     categories: string[]
+    selectedCategory: string
+    onSelect: (category: string) => void
 }
 
-export function CategoryPills({ categories }: CategoryPillsProps) {
+export function CategoryPills({ categories, selectedCategory, onSelect }: CategoryPillsProps) {
     return <div className="overflow-x-hidden relative">
         <div className="flex whitespace-nowrap gap-2 transition-transform w-[max-content]">
             {categories.map(category => (
-                <Button key={category} variant="dark" className="py-1 px-3 rounded-lg whitespace-nowrap">{category}</Button>
+                <Button key={category} variant={selectedCategory === category? "dark" : "default"} className="py-1 px-3 rounded-lg whitespace-nowrap">{category}</Button>
             ))}
         </div>
     </div>
