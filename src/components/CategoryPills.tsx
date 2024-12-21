b2c5f3e68624fa1f5e5125182ba52d3f8077ecd9
Arjun <arjunsemwal02@gmail.com>
2024-12-21 22:11:17 +0530

    the left and right chevron button for categories are done.

--- src/components/CategoryPills.tsx
+++ src/components/CategoryPills.tsx
@@ -1,5 +1,6 @@
-import { ChevronLeft } from "lucide-react";
+import { ChevronLeft, ChevronRight } from "lucide-react";
 import { Button } from "./Button";
+import { useEffect, useRef, useState } from "react";
 
 type CategoryPillsProps = {
     categories: string[]
@@ -7,9 +8,38 @@ type CategoryPillsProps = {
     onSelect: (category: string) => void
 }
 
+const TRANSLATE_AMOUNT = 200
+
 export function CategoryPills({ categories, selectedCategory, onSelect }: CategoryPillsProps) {
-    return <div className="overflow-x-hidden relative">
-        <div className="flex whitespace-nowrap gap-2 transition-transform w-[max-content]">
+    const [translate, setTranslate] = useState(0)
+    const [isLeftVisible, setIsLeftVisible] = useState(false)
+    const [isRightVisible, setIsRightVisible] = useState(false)
+    const containerRef = useRef<HTMLDivElement>(null)
+
+    useEffect(() => {
+        if(containerRef.current == null) return
+
+        const observer = new ResizeObserver(entries => {
+            const container = entries[0]?.target
+
+            if(container == null) return
+
+            setIsLeftVisible(translate > 0)
+            setIsRightVisible(translate + container.clientWidth < container.scrollWidth)
+        })
+
+        observer.observe(containerRef.current)
+
+        return () => {
+            observer.disconnect()
+        }
+
+    }, [categories, translate])
+
+    return <div ref={containerRef} className="overflow-x-hidden relative">
+        <div
+        className="flex whitespace-nowrap gap-2 transition-transform w-[max-content]" 
+        style={{ transform:`translateX(-${translate}px)`}}>
             {categories.map(category => (
                 <Button 
                 key={category} 
@@ -20,10 +50,39 @@ export function CategoryPills({ categories, selectedCategory, onSelect }: Catego
                 </Button>
             ))}
         </div>
-        <div>
-            <Button variant="ghost" size="icon" className="">
+        {isLeftVisible && (<div className="absolute left-0 top-1/2 -translate-y-1/2 bg-gradient-to-r from-white from-50% to-transparent w-25 h-full">
+            <Button 
+                onClick={() => {
+                    setTranslate(translate => {
+                        const newTranslate = translate - TRANSLATE_AMOUNT
+                        if(newTranslate <= 0) return 0
+                        return newTranslate
+                    })
+                }} 
+                variant="ghost" 
+                size="icon" 
+                className="h-full aspect-square w-auto p-1.5">
                 <ChevronLeft />
             </Button>
-        </div>
+        </div>)}
+        {isRightVisible && (<div className="absolute right-0 top-1/2 -translate-y-1/2 bg-gradient-to-l from-white from-50% to-transparent w-25 h-full flex justify-end">
+            <Button 
+                onClick={() => {setTranslate(translate => {
+                        if(containerRef.current == null) return translate
+                        const newTranslate = translate + TRANSLATE_AMOUNT
+                        const edge = containerRef.current.scrollWidth
+                        const width = containerRef.current.clientWidth
+                        if(newTranslate + width >= edge) {
+                            return edge - width
+                        }
+                        return newTranslate
+                    })
+                }} 
+                variant="ghost" 
+                size="icon" 
+                className="h-full aspect-square w-auto p-1.5">
+                <ChevronRight />
+            </Button>
+        </div>)}
     </div>
 }
