d3abbadd824f6b2fd201a1749e892a55ed66962e
Arjun <arjunsemwal02@gmail.com>
2025-01-03 13:41:40 +0530

    added the playlists and subscriptions section to the Sidebar

---
+++ src/data/sidebar.ts
@@ -0,0 +1,70 @@
+export const subscriptions = [
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+    {
+        channelName: "Arjun Semwal",
+        id: "arjunsemwal",
+        imgUrl: "https://yt3.googleusercontent.com/_KaiqDfA4YDqs5Eh79uhj2hl878x5yaeePGPLS8t6DpPTUNFeQreK61NlqncfVN2Sv0ioAsT_Q=s160-c-k-c0x00ffffff-no-rj"
+    },
+]
+
+export const playlists = [
+    {
+        id: 0,
+        name: "short films"
+    },
+    {
+        id: 1,
+        name: "feature films"
+    },
+    {
+        id: 2,
+        name: "tutorials"
+    },
+    {
+        id: 3,
+        name: "software development"
+    },
+    {
+        id: 4,
+        name: "workout"
+    }
+]
--- src/layouts/Sidebar.tsx
+++ src/layouts/Sidebar.tsx
@@ -1,30 +1,40 @@
-import { ChevronDown, ChevronUp, Clapperboard, Clock, Home, Library, History, PlaySquare, Repeat } from "lucide-react";
+import { ChevronDown, ChevronUp, Clapperboard, Clock, Home, Library, History, PlaySquare, Repeat, ListVideo } from "lucide-react";
 import { Button, buttonStyles } from "../components/Button";
 import { Children, ElementType, ReactNode, useState } from "react";
 import { twMerge } from "tailwind-merge";
+import { playlists, subscriptions } from "../data/sidebar";
 
 export function Sidebar() {
     return (
     <>
         <aside className="sticky top-0 overflow-y-auto scrollbar-hidden pb-5 flex flex-col ml-2 lg:hidden">
-            <SmallSidebarItem Icon={Home} title="Home" url="/"/>
-            <SmallSidebarItem Icon={Repeat} title="Shorts" url="/shorts"/>
-            <SmallSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subscriptions"/>
-            <SmallSidebarItem Icon={Library} title="Library" url="/library"/>
+            <SmallSidebarItem IconOrImgUrl={Home} title="Home" url="/"/>
+            <SmallSidebarItem IconOrImgUrl={Repeat} title="Shorts" url="/shorts"/>
+            <SmallSidebarItem IconOrImgUrl={Clapperboard} title="Subscriptions" url="/subscriptions"/>
+            <SmallSidebarItem IconOrImgUrl={Library} title="Library" url="/library"/>
         </aside>
         <aside className="w-60 lg:sticky absolute top-0 overflow-y-auto scrollbar-hidden pb-3 flex flex-col gap-2 px-2">
             <LargeSidebarSection visibleItemCount={3}>
-                <LargeSidebarItem isActive Icon={Home} title="Home" url="/" />
-                <LargeSidebarItem Icon={Repeat} title="Shorts" url="/shorts" />
-                <LargeSidebarItem Icon={Clapperboard} title="Subscriptions" url="/subscriptions" />
-                <LargeSidebarItem Icon={Library} title="Library" url="/library" />
+                <LargeSidebarItem isActive IconOrImgUrl={Home} title="Home" url="/" />
+                <LargeSidebarItem IconOrImgUrl={Repeat} title="Shorts" url="/shorts" />
+                <LargeSidebarItem IconOrImgUrl={Clapperboard} title="Subscriptions" url="/subscriptions" />
+                <LargeSidebarItem IconOrImgUrl={Library} title="Library" url="/library" />
             </LargeSidebarSection>
             <hr />
             <LargeSidebarSection visibleItemCount={5}>
-                <LargeSidebarItem Icon={Library} title="Library" url="/library" />
-                <LargeSidebarItem Icon={History} title="History" url="/history" />
-                <LargeSidebarItem Icon={PlaySquare} title="PlaySquare" url="/playsquare" />
-                <LargeSidebarItem Icon={Clock} title="Clock" url="/clock" />
+                <LargeSidebarItem IconOrImgUrl={Library} title="Library" url="/library" />
+                <LargeSidebarItem IconOrImgUrl={History} title="History" url="/history" />
+                <LargeSidebarItem IconOrImgUrl={PlaySquare} title="Freq Videos" url="/freqvideos" />
+                <LargeSidebarItem IconOrImgUrl={Clock} title="Watch Later" url="/watchlater" />
+                {playlists.map(playlist => (
+                    <LargeSidebarItem key={playlist.id} IconOrImgUrl={ListVideo} title={playlist.name} url={`/playlist?list=${playlist.id}`}/>
+                ))}
+            </LargeSidebarSection>
+            <hr />
+            <LargeSidebarSection visibleItemCount={6}>
+                {subscriptions.map(subs => (
+                    <LargeSidebarItem key={subs.id} IconOrImgUrl={`${subs.imgUrl}`} title={subs.channelName} url={``}/>
+                ))}
             </LargeSidebarSection>
         </aside>
     </>
@@ -32,22 +42,22 @@ export function Sidebar() {
 }
 
 type SidebarItemProps = {
-    Icon: ElementType
+    IconOrImgUrl: ElementType | string
     title: string
     url: string
     isActive?: boolean
 }
 
-function SmallSidebarItem({Icon, title, url}: SidebarItemProps) {
+function SmallSidebarItem({IconOrImgUrl, title, url}: SidebarItemProps) {
     return <a href={url} className={twMerge(buttonStyles({variant: "ghost"}), "px-1 py-5 flex flex-col items-center rounded-lg gap-1")}>
-            <Icon className="h-6 w-6"/>
+            <IconOrImgUrl className="h-6 w-6"/>
             <div className="text-sm">{title}</div>
         </a>
 }
 
-function LargeSidebarItem({Icon, title, url, isActive=false}: SidebarItemProps) {
+function LargeSidebarItem({IconOrImgUrl, title, url, isActive=false}: SidebarItemProps) {
     return <a href={url} className={twMerge(buttonStyles({variant: "ghost"}), `w-full flex items-center rounded-lf gap-3 p-3 ${isActive ? "font-bold bg-gray-100 hover:bg-secondary" : undefined }`)}>
-        <Icon className="w-6 h-6"/>
+        <IconOrImgUrl className="w-6 h-6"/>
         <div className="whitespace-nowrap overflow-hidden text-ellipsis">{title}</div>
     </a>
 }
